f2c8734684e363420a09708f508fc9d602ef1ac7
robakerson <robakerson@gmail.com>
2021-07-24 20:13:14 -0500

    Done?

---
+++ Blurry Loading/script.js
@@ -0,0 +1,27 @@
+const loadText= document.querySelector('.loading-text');
+const bg= document.querySelector('.bg');
+
+let load = 0;
+let int = setInterval(blurring, 50);
+let filterBlur= 50;
+
+
+function blurring(){
+  load++;
+    if(load>99){
+      clearInterval(int);
+    }
+
+    loadText.innerText = load +'%';
+    loadText.style.opacity = 1 - (load/100);
+
+    // Doesnt work!
+    // filterBlur -=0.3;
+    // bg.style.filter = 'blur(${filterBlur}px)';
+
+    // Doesn't work!
+    bg.style.filter= 'blur(${scale(load, 0, 100, 30, 0)}px)';
+    bg.style.filter= 'blur('+ 30-(30*(load/100)) +'px)';
+    bg.style.filter= 'blur('+ (30-(30*(load/100))) +'px)';
+    console.log(bg.style.filter);
+}
