8afdec01f7092197ea901f890c9eb777ab505c3d
robakerson <robakerson@gmail.com>
2021-08-09 22:58:01 -0500

    Done!

---
+++ Animated Navigation/script.js
@@ -0,0 +1,7 @@
+
+const toggle = document.getElementById('toggle');
+const nav = document.getElementById('nav');
+
+toggle.addEventListener('click', function(){
+  nav.classList.toggle('active');
+});
